d20b5c5756de1a919ee25b849d9a4aba8ef103d0
VaibhavRaut123 <135004181+VaibhavRaut123@users.noreply.github.com>
2023-05-31 19:01:52 +0530

    day2 my sir g videa yt

---
+++ input.py
@@ -0,0 +1,18 @@
+'''
+input programme
+'''
+"""print("Enter your number")
+a=int(input())
+b=int(input())
+c=a+b
+print("sum is",c)
+x=10
+print(x)"""
+a=10
+b=15
+c=5
+#print("hellow",a,'something',b)
+#print(a,b,c)
+#print(a,b,c,sep="-")
+print(a,b,c,sep="-",end='$')
+print("hellow")
